38e0b2ab90781f211b56822af14f446a49a14960
Kenta Merry <merryk69@gmail.com>
2024-05-28 18:03:59 +0200

    Add validations on game model + relation to rentals

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+  validates :name, :price, :platform, :genre, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
+  # belongs_to :user
+  has_many :rentals, dependent: :destroy
 end
