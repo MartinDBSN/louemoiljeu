f5c1880427d1c810848256a30a32ef5226b82c80
Martin Dubuisson <martin.dbssn@gmail.com>
2024-05-30 14:27:59 +0200

    Validation to the rental model added

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,7 @@
 class Rental < ApplicationRecord
   belongs_to :game
   belongs_to :user
+
+  validates :starting_date, :ending_date, presence: true
+  validates :ending_date, comparison: { greater_than: :starting_date }
 end
