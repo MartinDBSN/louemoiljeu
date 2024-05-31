2dae7cb72d81f3441a66143281dfbcb8e7cc8578
Hugo Nieuwenhuyse <hugo.nieuwenhuyse@gmail.com>
2024-05-31 15:37:56 +0200

    Done with the pictures of the games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 Game.destroy_all
 User.destroy_all
 p "Database cleaned Peeps"
+require "open-uri"
+
 
 users = [
   {
@@ -27,7 +29,6 @@ games = [
     genre: "FPS",
     price: 5,
     user_id: User.last.id },
-    photo: "https://image.jeuxvideo.com/images-sm/p2/m/o/mohfp20f.jpg"
   },
   { game: {
     name: "Regarde les chiens",
@@ -35,7 +36,6 @@ games = [
     genre: "Action-RPG",
     price: 7,
     user_id: User.last.id },
-    photo: "https://m.media-amazon.com/images/I/71X5SqK1ZeS._AC_UF1000,1000_QL80_.jpg"
   },
   { game: {
     name: "L'appel du devoir 2",
@@ -43,7 +43,6 @@ games = [
     genre: "FPS",
     price: 100,
     user_id: User.last.id },
-    photo: "https://upload.wikimedia.org/wikipedia/en/d/d1/Call_of_Duty_2_Box.jpg"
   },
   { game: {
     name: "Besoin de vitesse asphalte",
@@ -51,7 +50,6 @@ games = [
     genre: "Course",
     price: 12,
     user_id: User.last.id },
-    photo: "https://media.s-bol.com/qON2J2vwLQ7/962x1200.jpg"
   },
   { game: {
     name: "Habibtant maleveillant 4",
@@ -59,7 +57,6 @@ games = [
     genre: "TPS",
     price: 6,
     user_id: User.last.id },
-    photo: "https://m.media-amazon.com/images/I/41vi80KPaWL._AC_SY780_.jpg"
   },
   { game: {
     name: "007 oeil doré",
@@ -67,7 +64,6 @@ games = [
     genre: "Course",
     price: 16,
     user_id: User.last.id },
-    photo: "https://m.media-amazon.com/images/I/61ABkSW1grS._AC_UF1000,1000_QL80_.jpg"
   },
   { game: {
     name: "Artisanat minier",
@@ -75,12 +71,23 @@ games = [
     genre: "Survivre",
     price: 22,
     user_id: User.last.id },
-    photo: "https://image.jeuxvideo.com/medias/148285/1482845269-1018-jaquette-avant.jpg"
   }
 ]
 
-games.each do |game|
-  Game.create!(game)
+images  = ["https://image.jeuxvideo.com/images-sm/p2/m/o/mohfp20f.jpg",
+  "https://m.media-amazon.com/images/I/71X5SqK1ZeS._AC_UF1000,1000_QL80_.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/d/d1/Call_of_Duty_2_Box.jpg",
+  "https://media.s-bol.com/qON2J2vwLQ7/962x1200.jpg",
+  "https://m.media-amazon.com/images/I/41vi80KPaWL._AC_SY780_.jpg",
+  "https://m.media-amazon.com/images/I/61ABkSW1grS._AC_UF1000,1000_QL80_.jpg",
+  "https://image.jeuxvideo.com/medias/148285/1482845269-1018-jaquette-avant.jpg"
+]
+
+games.each_with_index do |game, i|
+  game = Game.create!(game[:game])
+  p game
+  image = URI.open(images[i])
+  game.photo.attach(io: image, filename: "#{game.name}.jpg")
 end
 
 puts "Created #{Game.count} games!"
