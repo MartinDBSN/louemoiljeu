05563fd77b0991d62879b47aef1d9b308c1ff7df
Martin Dubuisson <martin.dbssn@gmail.com>
2024-05-31 16:20:57 +0200

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,6 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'rentals#index', as: :dashboard
   post 'games/:game_id/rentals', to: 'rentals#create', as: :game_rentals
 
-
-
-  post 'games/:game_id/rentals', to: 'rentals#create', as: :game_rentals
-  get 'users/:id/rentals', to: 'pages#user', as: :rentals
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
